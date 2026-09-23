023bbd0fd4c370d4b323afa5fb8228d2aca0fa7a
rgarcia784-alt <rgarcia784@gmail.com>
2026-09-23 17:58:13 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOjPtqzQ8b7ZeMror015dpcuQCMrYWa1U",
+  authDomain: "uno-brokers-registro.firebaseapp.com",
+  projectId: "uno-brokers-registro",
+  storageBucket: "uno-brokers-registro.firebasestorage.app",
+  messagingSenderId: "272579298913",
+  appId: "1:272579298913:web:967a6b75d1c40760f34c20"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
